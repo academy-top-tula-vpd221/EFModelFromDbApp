dcbff13be25ef8ccb799d2d7b03a036f52a1e0f9
Ефимов Максим <efimov_m@itstep.academy>
2023-10-01 16:48:50 +0300

    Добавьте файлы проекта.

---
+++ EFModelFromDbApp/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFModelFromDbApp
+{
+    public partial class Company
+    {
+        public Company()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ EFModelFromDbApp/ef_dbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFModelFromDbApp
+{
+    public partial class ef_dbContext : DbContext
+    {
+        public ef_dbContext()
+        {
+        }
+
+        public ef_dbContext(DbContextOptions<ef_dbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Company> Companies { get; set; } = null!;
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ef_db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>(entity =>
+            {
+                entity.ToTable("Company");
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasIndex(e => e.CompanyId, "IX_Employees_CompanyId");
+
+                entity.HasOne(d => d.Company)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.CompanyId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
